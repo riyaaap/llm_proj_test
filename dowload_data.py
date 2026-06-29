6256420488e9ef5e24712199f85e0dd8edca0e0a
riyaaap <riya.patil1205@gmail.com>
2026-06-29 16:05:28 -0400

    added data loading python script

---
+++ dowload_data.py
@@ -0,0 +1,9 @@
+import os
+from datasets import load_dataset
+# Login using e.g. `huggingface-cli login` to access this dataset
+ds = load_dataset("jd5697/wikipedia-biology")
+curr_dir = os.path.dirname(os.path.abspath(__file__))
+save_path = os.path.join(curr_dir, "data")
+ds.save_to_disk(save_path)
+print(f"Dataset successfully saved to: {save_path}")
+
