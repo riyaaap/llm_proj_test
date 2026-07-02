02e40dde0c0d347532ad50168a76f0d205808fa5
riyaaap <riya.patil1205@gmail.com>
2026-07-02 13:14:03 -0400

    updated viewdata.py with generalizable function

--- viewdata.py
+++ viewdata.py
@@ -1,11 +1,18 @@
 import os
 from datasets import load_from_disk
 curr_dir = os.path.dirname(os.path.abspath(__file__))
-save_dir = os.path.join(curr_dir, "data")
 
-dataset = load_from_disk(save_dir)
-print(dataset)
-print(dataset[0])
-print(dataset[0:15])
+biofolder = "data"
+cnnfolder = "cnn_data"
+
+def view(data_folder):
+    save_dir = os.path.join(curr_dir, data_folder)
+
+    dataset = load_from_disk(save_dir)
+    print(dataset)
+    print(dataset[0])
+    print(dataset[0:15])
 
 
+view(biofolder)
+view(cnnfolder)
